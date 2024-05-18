d0e015e307d41a740f4344c5a4b5e086b193aa81
teranum <tera63614@gmail.com>
2024-05-18 15:04:50 +0900

    update sample 05

--- Python/05. 해외주식 종목조회.py
+++ Python/05. 해외주식 종목조회.py
@@ -16,11 +16,36 @@ async def main():
                 'InputDataCode': 시장분류코드,
             }
         }
-        response = await api.request('FSTKCODES', request)
-        if response is None:
-            print(f'요청실패: {api.last_message}')
-        else:
-            print_table(response.body['Out'])
+        
+        alldata = []
+        cont_yn = 'N'
+        cont_key = ''
+        req_count = 0
+        while True:
+            req_count += 1
+            print(f'요청횟수: {req_count}')
+            
+            response = await api.request('FSTKCODES', request, cont_yn=cont_yn, cont_key=cont_key)
+            if response is None:
+                print(f'요청실패: {api.last_message}')
+                break
+            else:
+                if 'Out' not in response.body:
+                    print(f'응답데이터 없음: {response.body}')
+                    break
+                alldata += response.body['Out']
+                cont_yn = response.cont_yn
+                cont_key = response.cont_key
+                if cont_yn != 'Y':
+                    break
+                recv_data_count = len(response.body['Out'])
+                if recv_data_count != 400: # 수신갯수가 400개가 아닐시 루프종료
+                    break
+            
+            await asyncio.sleep(0.5) # 초당제한 횟수 2회, 적당히 딜레이 준다
+            pass # end while, 요청횟수만큼 반복
+    
+        print_table(alldata)
             
         pass # end while, 시장분류코드 입력받기 위한 무한루프
     
@@ -33,7 +58,19 @@ asyncio.run(main())
 # Output:
 '''
 시장분류코드를 입력하세요 (NY: 뉴욕, NA: 나스닥, AM: 아멕스): NA
-Row Count = 400
+요청횟수: 1
+요청횟수: 2
+요청횟수: 3
+요청횟수: 4
+요청횟수: 5
+요청횟수: 6
+요청횟수: 7
+요청횟수: 8
+요청횟수: 9
+요청횟수: 10
+요청횟수: 11
+요청횟수: 12
+Row Count = 4487
 +-------+--------------------------------------------------------------+------------------+--------------+-------------+-------------+
 |  Iscd |                           KorIsnm                            |   BstpLargName   | ExchClsCode2 | SelnVolUnit | ShnuVolUnit |
 +-------+--------------------------------------------------------------+------------------+--------------+-------------+-------------+
@@ -53,16 +90,22 @@ Row Count = 400
 |  AAPU |              DIREXION DAILY AAPL BULL 2X SHARES              |                  |      FN      |      1      |      1      |
 |  AAXJ |       ISHARES TRUST MSCI ALL COUNTRY ASIA EX JAPAN ETF       |                  |      FN      |      1      |      1      |
 |  ABAT |                  아메리칸 배터리 테크놀로지                  |       소재       |      FN      |      1      |      1      |
-|  ABCB |                       아메리스 뱅코프                        |       금융       |      FN      |      1      |      1      |
-|  ABCL |                    앱셀레라 바이오로직스                     |     헬스케어     |      FN      |      1      |      1      |
-|  ABCS |   EA SERIES TRUST ALPHA BLUE CAP US SM-MID CAP DYNAMIC ETF   |                  |      FN      |      1      |      1      |
-|  ABEO |                     아베오나 테라퓨틱스                      |     헬스케어     |      FN      |      1      |      1      |
-|  ABIO |                       ARCA 바이오파머                        |     헬스케어     |      FN      |      1      |      1      |
-|  ABL  |                       애버커스 라이프                        |       금융       |      FN      |      1      |      1      |
-| ABLLL |        애버커스 라이프 선순위채권(2028-11-15 9.875%)         |                  |      FN      |      1      |      1      |
-|  ABLV |                       에이블 뷰 글로벌                       |      산업재      |      FN      |      1      |      1      |
-|  ABNB |                          에어비앤비                          |   경기 소비재    |      FN      |      1      |      1      |
-|  ABOS |                     애커먼 파머슈티컬스                      |     헬스케어     |      FN      |      1      |      1      |
-|  ABSI |                            압사이                            |     헬스케어     |      FN      |      1      |      1      |
 ...
+...
+...
+...
+|  ZNTL |                     젠테일스 파머슈티컬                      |     헬스케어     |      FN      |      1      |      1      |
+|  ZOOZ |                          주즈 파워                           |        IT        |      FN      |      1      |      1      |
+|  ZPTA |                     저파타 컴퓨팅 홀딩스                     |        IT        |      FN      |      1      |      1      |
+|   ZS  |                          지스케일러                          |        IT        |      FN      |      1      |      1      |
+|  ZTEK |                             젠텍                             |       소재       |      FN      |      1      |      1      |
+|  ZUMZ |                           주미에즈                           |   경기 소비재    |      FN      |      1      |      1      |
+|  ZURA |                         주라 바이오                          |     헬스케어     |      FN      |      1      |      1      |
+| ZURAW |              주라 바이오 콜 워런트(2028-03-20)               |                  |      FN      |      1      |      1      |
+|  ZVRA |                      지브러 테라퓨틱스                       |     헬스케어     |      FN      |      1      |      1      |
+|  ZVSA |                     자이버사 테라퓨틱스                      |     헬스케어     |      FN      |      1      |      1      |
+|  ZYME |                           자임웍스                           |     헬스케어     |      FN      |      1      |      1      |
+|  ZYXI |                            지넥스                            |     헬스케어     |      FN      |      1      |      1      |
+|  ZZZ  |     ONEFUND TRUST CYBER HORNET S&P 500 & BITCOIN 75/25 S     |                  |      FN      |      1      |      1      |
++-------+--------------------------------------------------------------+------------------+--------------+-------------+-------------+
 '''
